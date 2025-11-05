98984adac2f6eb21422352fab3563f79f99dfc91
hamnakhan10000 <hamnakhan10000@gmail.com>
2025-11-05 13:45:23 -0600

    Initial commit for frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "./Menu";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>☕ Coffee Shop Menu</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
